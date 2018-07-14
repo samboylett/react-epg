41afb419d345fdfbb0b4b54e3766ec599f014d68
Sam Boylett <sam.boylett@piksel.com>
2018-07-14 14:41:07 +0100

    fills in timeslots

--- test/spec/Channel.js
+++ test/spec/Channel.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Enzyme, { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import 'jest-enzyme';
-import { Channel } from '../../lib/index';
+import { Channel, TimeSlot } from '../../lib/index';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -13,13 +13,35 @@ describe('Channel', () => {
   const setupComponent = (props = {}) => {
     component = mount((
       <Channel name="Foo Bar" {...props}>
-        {props.children || (<p>TimeSlots</p>)}
+        {props.children || (<TimeSlot start={0} end={10}>Bah</TimeSlot>)}
       </Channel>
     ));
   };
 
   const tr = () => component.find('tr');
   const th = () => tr().find('th');
+
+  describe('when time slots not joined up', () => {
+    beforeEach(() => setupComponent({
+      children: [
+        <TimeSlot
+          start={new Date('1/1/97 14:00')}
+          end={new Date('1/1/97 14:59')}
+        >Foo</TimeSlot>,
+        <TimeSlot
+          start={new Date('1/1/97 16:00')}
+          end={new Date('1/1/97 17:00')}
+        >Bar</TimeSlot>
+      ]
+    }));
+
+    it('renders a time slot between them', () => {
+      const timeSlots = component.find(TimeSlot);
+      expect(timeSlots).toHaveLength(3);
+      expect(timeSlots.at(1)).toHaveProp('start', new Date('1/1/97 15:00'));
+      expect(timeSlots.at(1)).toHaveProp('end', new Date('1/1/97 15:59'));
+    });
+  });
 
   describe('basic structure', () => {
     beforeEach(setupComponent);
@@ -36,8 +58,8 @@ describe('Channel', () => {
     });
 
     it('renders children after the th', () => {
-      expect(tr().children().at(1)).toHaveText('TimeSlots');
-      expect(tr().find('p')).toHaveText('TimeSlots');
+      expect(tr().children().at(1)).toHaveText('Bah');
+      expect(tr().find(TimeSlot)).toHaveText('Bah');
     });
   });
 });
